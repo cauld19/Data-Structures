9165911b40a2a475742448f00865cbb0dcc5f515
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-08-01 12:36:10 -0700

    fixed contains in binary search

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -46,27 +46,14 @@ class BSTNode:
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        cur_node = self
-        if self is None:
-            return False
-        elif cur_node.value is target:
-            return True
-        else:
-            while cur_node:
-                if target < cur_node.value:
-                    if cur_node.left is None:
-                        return False
-                    elif target is not cur_node.left.value:
-                        cur_node = cur_node.left
-                    else:
-                        return True
-                else:
-                    if cur_node.right is None:
-                        return False
-                    elif target is not cur_node.right.value:
-                        cur_node = cur_node.right
-                    else:
-                        return True
+        while self is not None:
+            if target > self.value:
+                self = self.right
+            elif target < self.value:
+                self = self.left
+            else:
+                return True
+        return False
                     
         
         
